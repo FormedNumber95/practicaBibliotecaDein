247818d149e6cfb2fd085e2ab624b665a17a2b50
FormedNumber95 <aketza200510@gmail.com>
2025-01-18 12:36:18 +0100

    añadida la interfaz del alumno
    faltan tooltips

---
+++ src/main/java/es/aketzagonzalez/ctrl/AlumnosController.java
@@ -0,0 +1,83 @@
+package es.aketzagonzalez.ctrl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TextField;
+
+public class AlumnosController {
+
+    @FXML
+    private Button btnAniadir;
+
+    @FXML
+    private Button btnModificar;
+
+    @FXML
+    private Menu menAlumnos;
+
+    @FXML
+    private Menu menAyuda;
+
+    @FXML
+    private Menu menDevoluciones;
+
+    @FXML
+    private Menu menInformes;
+
+    @FXML
+    private Menu menLibros;
+
+    @FXML
+    private Menu menPrestamos;
+
+    @FXML
+    private TextField txtFiltro;
+
+    @FXML
+    void accionFiltrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modificarAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verAlumnos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verAyuda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verDevoluciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verInformes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verLibros(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verPrestamos(ActionEvent event) {
+
+    }
+
+}
