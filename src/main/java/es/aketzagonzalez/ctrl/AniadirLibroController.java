8ae5e7d4dc6d25ac3f1bbafc0fbbc0ee8825aa25
FormedNumber95 <aketza200510@gmail.com>
2025-01-24 18:53:37 +0100

    añadido el layout para añadir libros

---
+++ src/main/java/es/aketzagonzalez/ctrl/AniadirLibroController.java
@@ -0,0 +1,39 @@
+package es.aketzagonzalez.ctrl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AniadirLibroController {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private ComboBox<?> cmbEstado;
+
+    @FXML
+    private TextField txtAutor;
+
+    @FXML
+    private TextField txtEditorial;
+
+    @FXML
+    private TextField txtTitulo;
+
+    @FXML
+    void accionCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionGuardar(ActionEvent event) {
+
+    }
+
+}
